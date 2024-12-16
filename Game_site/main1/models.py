2dedba884d308a46df58e09fc1ab4afd23a3b97c
FastFox123414 <98419590+FastFox123414@users.noreply.github.com>
2024-12-16 18:59:58 +0300

    Add files via upload

--- Game_site/main1/models.py
+++ Game_site/main1/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class New(models.Model):
+    title = models.CharField("Название", max_length=40)
+    anons = models.CharField("Анонс", max_length=250)
+    foto = models.ImageField("Картинка", null= True, upload_to="main1/static/imags")
+    full_text  = models.TextField("Основной текст")
+    date = models.DateTimeField("Дата публикайии")
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name="Новость"
+        verbose_name_plural="Новости"
