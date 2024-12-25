7208c0cd27a8a2f545d58b73d3061a61ddd4eb58
FastFox123414 <98419590+FastFox123414@users.noreply.github.com>
2024-12-25 19:19:06 +0300

    Add files via upload

--- Game_site/main1/models.py
+++ Game_site/main1/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class New(models.Model):
     title = models.CharField("Название", max_length=40)
     anons = models.CharField("Анонс", max_length=250)
-    foto = models.ImageField("Картинка", null= True, upload_to="main1/static/imags")
+    foto = models.ImageField("Картинка", null= True, upload_to="static/main1/imags")
     full_text  = models.TextField("Основной текст")
     date = models.DateTimeField("Дата публикайии")
     def __str__(self):
@@ -15,7 +15,7 @@ class New(models.Model):
         
 class New_Game(models.Model):
     title = models.CharField("Название игры", max_length=20)
-    foto = models.ImageField("Картинка", null= True, upload_to="main1/static/imags")
+    foto = models.ImageField("Картинка", null= True, upload_to="static/main1/imags")
     full_text  = models.CharField("Небольшое описание",max_length=40)
     date = models.DateTimeField("Дата анонса")
     def __str__(self):
@@ -27,7 +27,7 @@ class New_Game(models.Model):
         
 class Reviews(models.Model):
     title = models.CharField("Название игры", max_length=30)
-    foto = models.ImageField("Картинка", null= True, upload_to="main1/static/imags")
+    foto = models.ImageField("Картинка", null= True, upload_to="static/main1/imags")
     full_text  = models.TextField("Описание")
     link = models.TextField("Ссылка на видео")
     def __str__(self):
@@ -39,7 +39,7 @@ class Reviews(models.Model):
         
 class Ratings(models.Model):
     title = models.CharField("Название игры",max_length=40)
-    foto = models.ImageField("Картинка", null= True, upload_to="main1/static/imags")
+    foto = models.ImageField("Картинка", null= True, upload_to="main1/static/main1/imags")
     full_text  = models.CharField("Небольшое описание",max_length=40)
     rating= models.FloatField("Оценка до 2 знаков после запятой")
     def __str__(self):
